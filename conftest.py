48f0d4c5a6bc512447b8db61b2729c526a7ff0dc
Aliaksandr <frombywithlove@gmail.com>
2024-06-08 23:52:19 +0300

    for tests on Docker and Jenkins(Options)

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def driver():
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
--- pages/base_page.py
+++ pages/base_page.py
@@ -10,7 +10,7 @@ class BasePage:
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
-        self.wait_5s = WebDriverWait(self.driver, 5)
+        self.wait_5s = WebDriverWait(self.driver, 10)
 
     @allure.step('Open the page')
     def open(self):
