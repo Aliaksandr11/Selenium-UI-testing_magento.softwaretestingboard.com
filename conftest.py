be513ee8c28b4b9a9e04c12752bf5e4b731875e8
Aliaksandr <frombywithlove@gmail.com>
2024-06-08 19:23:44 +0300

    for tests on Docker and Jenkins

--- conftest.py
+++ conftest.py
@@ -1,9 +1,9 @@
 import pytest
 from selenium import webdriver
 
-from test_UI_altsvetkov_selenium.pages.create_account_page import CreateAccountPage
-from test_UI_altsvetkov_selenium.pages.eco_friendly_collection_page import EcoCollectionPage
-from test_UI_altsvetkov_selenium.pages.sale_page import SalePage
+from pages.create_account_page import CreateAccountPage
+from pages.eco_friendly_collection_page import EcoCollectionPage
+from pages.sale_page import SalePage
 
 
 @pytest.fixture()
