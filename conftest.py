deb2f83e434e0af39dda61436da2a36379cc9da3
Aliaksandr <frombywithlove@gmail.com>
2024-06-08 23:32:37 +0300

    for tests on Docker and Jenkins(Options)

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from pages.create_account_page import CreateAccountPage
 from pages.eco_friendly_collection_page import EcoCollectionPage
@@ -8,7 +9,11 @@ from pages.sale_page import SalePage
 
 @pytest.fixture()
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
